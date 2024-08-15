e99124719946f2477b32a2fbd175af26e4a2b4ca
Natto Tsai <fracture_star@hotmail.com>
2024-08-15 23:40:08 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <script setup>
 import { ref } from 'vue'
 
-const num = ref(1)
+const array = ref([
+  { name: '小明', age: 20 },
+  { name: '小藏', age: 30 },
+  { name: '小王', age: 25 },
+  { name: '小小', age: 60 }
+])
 
-const clickMe = () => {
-  num.value++
-}
+const isChecked = ref(false)
 </script>
 
 <template>
-  {{ num }}
+  <div v-for="({ name, age }, key) in array" :key="key">
+    索引：{{ key }} 姓名：{{ name }} 年齡：{{ age }}
+    <div v-if="age > 25">老來放</div>
+    <hr />
+  </div>
 
-  <button type="button" @click="clickMe">點我</button>
+  <input type="checkbox" v-model="isChecked" />{{ isChecked }}
+
+  <div v-if="isChecked">YYYYYYYYYYY</div>
+  <div v-else>NNNNNNNNNNNN</div>
 </template>
+
+<style></style>
